a2015c2be9f26e1a44edf5d7c7af988f043bf8d4
TranVinh-ds <chenvinh2003@gmail.com>
2023-02-27 10:24:39 +0700

    save listings to firestore

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -6,6 +6,7 @@ import {
   uploadBytesResumable,
   getDownloadURL,
 } from 'firebase/storage';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
@@ -138,7 +139,6 @@ const CreateListing = () => {
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
     }
 
     //store image in firebase
@@ -189,8 +189,23 @@ const CreateListing = () => {
       toast.error('Images not uploaded');
       return;
     });
-    console.log(imageUrls);
+
+    const formDataCopy = {
+      ...formData,
+      imageUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+    };
+
+    formDataCopy.location = address;
+    delete formDataCopy.images;
+    delete formDataCopy.address;
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+
+    const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
     setLoading(false);
+    toast.success('Listing saved');
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
   };
   return (
     <div className="profile">
